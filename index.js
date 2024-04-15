f76acb2af5791e9a5b0959b0d8b729aa548e75f4
tonniegeetz <tonniegeetz@gmail.com>
2024-04-15 23:18:14 +0300

    Understand how to create delete and append node elements

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion!';
+document.body.appendChild(newHeader);
